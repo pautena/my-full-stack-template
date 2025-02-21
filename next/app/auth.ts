83a7437c272eccf2bd0b18770b1c86fadbd6233a
Pau Tena Vidal <me@pautena.com>
2025-02-21 22:31:49 +0100

    Setup AuthJS (#3)
    * Setup a basic AuthJS Credentials configuration
    
    * ✨ Autogenerate frontend client
    
    * Fix next-auth setup to work with @hey-api/client-fetch
    
    * Implement authorize function
    
    * Add middleware to check when the user has sign in
    
    ---------
    
    Co-authored-by: Pau Tena <me@protonmail.com>
    Co-authored-by: github-actions <github-actions@github.com>

---
+++ next/app/auth.ts
@@ -0,0 +1,57 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+import { LoginService, UsersService, client } from "./client"
+import { z } from 'zod';
+
+const baseUrl = process.env.API_URL || "";
+client.setConfig({
+  baseUrl,
+})
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    authorized: ({auth})=> !!auth,
+  },
+  providers: [
+    Credentials({
+      credentials: {
+        email: { label: "Email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({ email: z.string().email(), password: z.string() })
+          .safeParse(credentials);
+
+        if(parsedCredentials.success){
+          const {email, password} = parsedCredentials.data;
+          const {data,response} = await LoginService.loginAccessToken({
+            body:{ username: email, password}
+          })
+
+          if(response.ok){
+
+            client.setConfig({
+              baseUrl,
+              headers:({
+                'Authorization':data && `Bearer ${data.access_token}`,
+              })
+            })
+
+            const {data:meData, response:meResponse} = await UsersService.readUserMe()
+            if(meResponse.ok){
+              return {
+                email: meData?.email,
+                name: meData?.full_name,
+              }
+            }
+          }
+        }
+
+
+
+        return null;
+      },
+    }),
+  ],
+})
