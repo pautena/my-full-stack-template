a52442d4bd9c6bb3092723c779438cb6acd2188f
Pau Tena <me@protonmail.com>
2025-03-02 16:17:58 +0100

    Add appbar menu

--- frontend/src/routes/_layout.tsx
+++ frontend/src/routes/_layout.tsx
@@ -1,8 +1,10 @@
 import { Outlet, createFileRoute, redirect } from "@tanstack/react-router"
 import useAuth, { isLoggedIn } from "../hooks/useAuth"
-import { Content, Drawer, DrawerAppBar, DrawerContent, DrawerLayout, DrawerNavigation, DrawerNavigationItem, DrawerNavigationItemLink, LoadingArea } from "@pautena/react-design-system"
+import { Drawer, DrawerAppBar, DrawerContent, DrawerLayout, LoadingArea } from "@pautena/react-design-system"
 import { useGetSidebarNav } from "../app/sidebar"
-
+import { Box, IconButton, Menu, MenuItem } from "@mui/material"
+import { useState } from "react"
+import AccountCircle from '@mui/icons-material/AccountCircle';
 
 export const Route = createFileRoute("/_layout")({
   component: Layout,
@@ -16,15 +18,60 @@ export const Route = createFileRoute("/_layout")({
 })
 
 function Layout() {
-  const { isLoading } = useAuth()
+  const { isLoading,logout } = useAuth()
   const sidebarNav = useGetSidebarNav();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    logout();
+    handleClose();
+  };
 
   return (
     <DrawerLayout drawerProviderProps={{variant:"mini"}}>
       <Drawer>
         <DrawerContent nav={sidebarNav}/>
       </Drawer>
-      <DrawerAppBar title="My Full Stack Template" />
+      <DrawerAppBar title="My Full Stack Template" >
+        <Box display="flex" justifyContent="flex-end" flexGrow={1}>
+          <IconButton
+            size="large"
+            aria-label="account of current user"
+            aria-controls="menu-appbar"
+            aria-haspopup="true"
+            onClick={handleMenu}
+            color="inherit"
+          >
+            <AccountCircle />
+          </IconButton>
+          <Menu
+            id="menu-appbar"
+            anchorEl={anchorEl}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
+        </Box>
+
+      </DrawerAppBar>
       {isLoading ? <LoadingArea/>:<Outlet/>}
     </DrawerLayout>
   )
