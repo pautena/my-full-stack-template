24c083aa7ad5dba97ecce741d12f4cad0592af36
Pau Tena Vidal <me@pautena.com>
2025-03-13 23:21:28 +0100

    Setup frontend testing (#27)
    * Basic testing library setup
    
    * Add GH action to test frontend
    
    * Add render function
    
    * update GH actions
    
    ---------
    
    Co-authored-by: Pau Tena <me@protonmail.com>

---
+++ frontend/src/test-utils.tsx
@@ -0,0 +1,18 @@
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { type RenderOptions, render } from "@testing-library/react";
+import type React from "react";
+import type { ReactElement } from "react";
+
+const theme = createTheme();
+
+const AllTheProviders = ({ children }: { children: React.ReactNode }) => {
+	return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+};
+
+const customRender = (
+	ui: ReactElement,
+	options?: Omit<RenderOptions, "wrapper">,
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from "@testing-library/react";
+export { customRender as render };
